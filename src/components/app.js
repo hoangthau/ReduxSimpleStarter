f25c1cc6c3f0cc6f6e028c6b9c41c84d20721164
Thau Nguyen <thau.nguyen@orientsoftware.net>
2018-07-04 17:22:26 +0700

    Get video list

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './SearchBar';
 
 const API_KEY = 'AIzaSyCe6k-m_QBnlkm3RxQiqWF3PNpLSostCro';
 
 export default class App extends React.Component {
+  state = {
+    videos: []
+  };
+
+  componentDidMount() {
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
